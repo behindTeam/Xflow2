79f4245326c07dd61ed31f0adb20bafc8552850b
kkh5535 <kkh5535@naver.com>
2023-12-11 17:12:58 +0900

    Modified: MyMqttMessage

--- src/main/java/com/front/message/MyMqttMessage.java
+++ src/main/java/com/front/message/MyMqttMessage.java
@@ -1,12 +1,17 @@
 package com.front.message;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 public class MyMqttMessage extends Message {
     byte[] payload;
     String topic;
     UUID senderId;
+    Map<String, Double> measurement = new HashMap<>();
+    Map<String, String> tags = new HashMap<>();
+    String deviceId = null;
 
     public MyMqttMessage(UUID senderId, String topic, byte[] payload) {
         this.payload = Arrays.copyOf(payload, payload.length);
@@ -24,6 +29,30 @@ public class MyMqttMessage extends Message {
 
     public String getTopic() {
         return topic;
+    }
+
+    public Map<String, String> getTags() {
+        return this.tags;
+    }
+
+    public void putTags(String title, String value) {
+        tags.put(title, value);
+    }
+
+    public Map<String, Double> getMeasurement() {
+        return this.measurement;
+    }
+
+    public void putMeasurement(String sensor, Double value) {
+        measurement.put(sensor, value);
+    }
+
+    public String getDeviceId() {
+        return this.deviceId;
+    }
+
+    public void setDeviceId(String deviceId) {
+        this.deviceId = deviceId;
     }
 
     @Override
