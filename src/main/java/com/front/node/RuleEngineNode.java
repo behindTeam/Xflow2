14e18158bf11da53b9145bcee29b81fc4584483d
{Danmoozzi} <{jaeyoon7421@naver.com}>
2023-12-13 13:20:01 +0900

    "[add] : 룰엔진노드 수정중"

--- src/main/java/com/front/node/RuleEngineNode.java
+++ src/main/java/com/front/node/RuleEngineNode.java
@@ -1,24 +1,17 @@
 package com.front.node;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Objects;
 import org.json.simple.JSONObject;
+import com.front.message.JsonMessage;
 import com.front.message.Message;
-import com.front.message.MyMqttMessage;
-import com.front.wire.Wire;
 
 public class RuleEngineNode extends InputOutputNode {
-    Wire outputWire;
-    Wire inputWire;
-    Map<Integer, JSONObject> database = new HashMap<>();
-    Message message;
 
-    public RuleEngineNode() {
+    RuleEngineNode() {
         this(1, 1);
     }
 
-    public RuleEngineNode(int inCount, int outCount) {
+    RuleEngineNode(int inCount, int outCount) {
         super(inCount, outCount);
     }
 
@@ -27,26 +20,25 @@ public class RuleEngineNode extends InputOutputNode {
         //
     }
 
-    // 여기서 해야할것은 jsonObject를 가져와서 mqtt로 오면 address파일 채워주고 // 나머지 반대로 하면 된다.
-    // 앞에 있는 인풋와이어에있는 모든 것을 체크해서 해보자.
-    // 메세지 프로세스를 만들었따
-
     @Override
     void process() {
-        // mqtt에서 주는 값 받기
+        if ((getInputWire(0) != null) && (getInputWire(0).hasMessage())) {
+            Message myMessage = getInputWire(0).get();
+            if (myMessage instanceof JsonMessage
+                    && (Objects.nonNull(((JsonMessage) myMessage).getPayload()))) {
+                msgParser((JsonMessage) myMessage);
+
+            }
+        }
     }
 
     @Override
-    synchronized void postprocess() {
+    void postprocess() {
         //
     }
 
-    @Override
-    public void run() {
-        preprocess();
-        process();
-        postprocess();
+    private void msgParser(JsonMessage myMessage) {
+        JSONObject payload = myMessage.getPayload();
     }
 
-    public void mqttIn
 }
