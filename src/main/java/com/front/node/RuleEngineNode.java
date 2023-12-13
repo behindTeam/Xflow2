278a69dc9d622120d98ea644423db8bbd8379946
{Danmoozzi} <{jaeyoon7421@naver.com}>
2023-12-13 16:55:19 +0900

    "파일 정리 한번 했어요"

--- src/main/java/com/front/node/RuleEngineNode.java
+++ src/main/java/com/front/node/RuleEngineNode.java
@@ -54,23 +54,57 @@ public class RuleEngineNode extends InputOutputNode {
             for (Object fromdatabaseskey : database.keySet()) {
                 if (fromdatabaseskey.toString().equals(key)) {
                     Map<String, Object> data = new HashMap<>();
-                    data.put(key, database.get(key));
+                    JSONObject target = (JSONObject) database.get(key);
+                    Object value = payload.get(key);
+                    target.replace("value", value);
+                    data.put(key, target);
+                    System.out.println("--------->  Mqtt 메시지 입니다.");
                     System.out.println(new JSONObject(data));
                     output(new JsonMessage(new JSONObject(data)));
+                } else if (key.equals("payload")) {
+                    JSONObject targetIn = (JSONObject) database.get(fromdatabaseskey.toString());
+                    JSONObject target = (JSONObject) targetIn.get("in");
+                    JSONObject recievePayload = (JSONObject) payload.get("payload");
+                    if (recievePayload.get("unitId").toString()
+                            .equals(target.get("unitId").toString())) {
+                        Map<String, Object> data = new HashMap<>();
+                        JSONObject outTarget =
+                                (JSONObject) database.get(fromdatabaseskey.toString());
+                        Object value = recievePayload.get("value");
+                        outTarget.replace("value", value);
+                        data.put(fromdatabaseskey.toString(), outTarget);
+                        System.out.println("--------->  Modbus 메시지 입니다.");
+                        System.out.println(new JSONObject(data));
+                        output(new JsonMessage(new JSONObject(data)));
+                    }
                 }
             }
 
         } catch (IOException | ParseException e) {
             e.printStackTrace();
         }
+
+
     }
 
     public static void main(String[] args) {
         RuleEngineNode node = new RuleEngineNode();
+        // --> MqttMessageParsing
         Map<String, Object> data1 = new HashMap<>();
-        data1.put("24e124785c389010-temperature", 13);
+        data1.put("24e124785c389010-temperature", 26);
         JSONObject data2 = new JSONObject(data1);
-        JsonMessage message = new JsonMessage(data2);
+        JsonMessage messageMqtt = new JsonMessage(data2);
+        node.msgParser(messageMqtt);
+
+
+        // Map<String,Object> data = new HashMap<>();
+        // Map<String,Object> payload = new HashMap<>();
+        JSONObject data = new JSONObject();
+        JSONObject payload = new JSONObject();
+        data.put("unitId", 1);
+        data.put("value", 66);
+        payload.put("payload", data);
+        JsonMessage message = new JsonMessage(new JSONObject(payload));
         node.msgParser(message);
     }
 
